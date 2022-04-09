4f6eb211a4297db47a175c64a1dc4bc7ccdaad9f
AlexanderDmitriev <alex9110077@gmail.com>
2022-04-09 20:09:50 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export class App extends Component {
   render() {
     
     return <div>
-          <p>Please leave feedback</p>
+          <Title>Please leave feedback</Title>
           <button type="button">GOOD</button>
           <p>Statistics</p>
           <ul>
